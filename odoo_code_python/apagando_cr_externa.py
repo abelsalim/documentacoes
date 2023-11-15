23e5caaece4714afed245f4a22f2fc51b1262caa
abelsalim <abelsalim@proton.me>
2023-11-15 10:21:20 -0300

    [ADD] deleta cr cobrança

---
+++ odoo_code_python/apagando_cr_externa.py
@@ -0,0 +1,31 @@
+from tqdm import tqdm
+
+def executa():
+    # selecionando produto
+    lotes = self.env['pedido.documento'].search(
+        [
+            ('operacao_id', '=', 89)
+        ]
+    )
+
+    # Itera em produtos no caso de cadastros duplicado no odoo
+    for lote in tqdm(lotes):
+        numero = lote.numero
+        print(f'lote: {numero}')
+
+        # Escreve dados
+        lote.unlink()
+        lote.flush()
+        self.env.cr.commit()
+
+        print(f'lote: {numero} - Apagado!')
+
+        print(55 * '-')
+
+        del numero
+
+    # Limpando cache
+    lote.invalidate_cache()
+
+
+executa()
