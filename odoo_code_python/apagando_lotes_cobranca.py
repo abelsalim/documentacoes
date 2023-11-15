bc08100f6b248956cc623aa70454c3bc4a0b4d27
abelsalim <abelsalim@proton.me>
2023-11-15 10:24:48 -0300

    [ADD] apaga lotes da cobrança

---
+++ odoo_code_python/apagando_lotes_cobranca.py
@@ -0,0 +1,25 @@
+def executa():
+    # selecionando produto
+    lotes = self.env['finan.lote.cobranca'].search()
+
+    # Itera em produtos no caso de cadastros duplicado no odoo
+    for lote in lotes:
+        numero = lote.numero
+        print(f'lote: {numero}')
+
+        # Escreve dados
+        lote.unlink()
+        lote.flush()
+        self.env.cr.commit()
+
+        print(f'lote: {numero} - Apagado!')
+
+        print(55 * '-')
+
+        del numero
+
+    # Limpando cache
+    lote.invalidate_cache()
+
+
+executa()
