c294760a5cfe6460efa6a479c05b27160da44578
abelsalim <abelsalim@proton.me>
2024-01-15 15:17:36 -0300

    [ADD] inclusão metodo escreve dados em csv

--- odoo_code_python/odoo_importacao/add/adiciona_cliente_spc.py
+++ odoo_code_python/odoo_importacao/add/adiciona_cliente_spc.py
@@ -1,12 +1,29 @@
 import csv
 
 
+def escreve_dados_csv(dados, nome_arquivo):
+    # Caminho padrão do arquivo
+    diretorio = '/home/zenir'
+
+    with open(f'{diretorio}/{nome_arquivo}', 'w', newline='') as arquivo_csv:
+        cabecalho = ['cnpj_cpf']
+        writer = csv.DictWriter(arquivo_csv, fieldnames=cabecalho, delimiter=',')
+
+        print('Escrevendo dados!')
+
+        writer.writeheader()
+        writer.writerows(dados)
+
+        print('Dados escritos!')
+
+
 def executa(arquivo):
     with open(arquivo, 'r') as arquivo_csv:
         arquivo_csv = csv.reader(arquivo_csv, delimiter=';')
         try:
+            lista_clientes_sem_registro = []
             for linha in arquivo_csv:
-                cnpj_cpf, contrato = linha
+                cnpj_cpf = ''.join(linha)
 
                 clientes = self.env['sped.participante'].search(
                     [('cnpj_cpf', '=', cnpj_cpf)]
@@ -14,6 +31,7 @@ def executa(arquivo):
 
                 # Sai se cliente não existe
                 if not clientes:
+                    lista_clientes_sem_registro.append({'cnpj_cpf': cnpj_cpf})
                     continue
 
                 for cliente in clientes:
@@ -40,6 +58,13 @@ def executa(arquivo):
 
                     cliente.invalidate_cache()
 
+            nome_arquivo = arquivo.split('/')[-1]
+            nome_arquivo = (
+                f'clientes_nao_encontrados_{nome_arquivo.split(".")[0]}.csv'
+            )
+
+            escreve_dados_csv(lista_clientes_sem_registro, nome_arquivo)
+
         except ValueError:
             print(ValueError)
 
@@ -47,4 +72,4 @@ def executa(arquivo):
             print(cliente.nome)
 
 
-executa('/tmp/spc.csv')
+executa('/tmp/spc/crateus.csv')
