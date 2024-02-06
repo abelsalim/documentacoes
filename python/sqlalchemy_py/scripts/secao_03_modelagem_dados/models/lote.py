98eba2dc5d748d5ca7176cf8939569caedc5c004
abelsalim <abelsalim@proton.me>
2024-02-06 05:59:47 -0300

    [ADD] models parte 2

---
+++ python/sqlalchemy_py/scripts/secao_03_modelagem_dados/models/lote.py
@@ -0,0 +1,44 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+
+from datetime import datetime
+from sqlalchemy_py.scripts.secao_03_modelagem_dados.models.models_base import (
+    ModelBase
+)
+from sqlalchemy_py.scripts.secao_03_modelagem_dados.models.tipo_picole import (
+    TipoPicoles
+)
+
+
+class Lote(ModelBase):
+    __tablename__: str = 'lotes'
+
+    id: int = sa.Column(
+        sa.BigInteger,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    data_criacao: datetime = sa.Column(
+        sa.DateTime,
+        default=datetime.now,
+        index=True
+    )
+
+    tipo_picole_id: int = sa.Column(
+        sa.Integer,
+        sa.ForeignKey('tipo_picole.id')
+    )
+
+    tipo_picole: TipoPicoles = orm.relationship(
+        'TipoPicole',
+        lazy='joined'
+    )
+
+    quantidade: str = sa.Column(
+        sa.Integer,
+        nullable=False
+    )
+
+    def __repr__(self) -> str:
+        return f'<Lote: {self.nome}>'
---
+++ python/sqlalchemy_py/scripts/secao_03_modelagem_dados/models/revendedor.py
@@ -0,0 +1,55 @@
+import sqlalchemy as sa
+
+from datetime import datetime
+from sqlalchemy_py.scripts.secao_03_modelagem_dados.models.models_base import (
+    ModelBase
+)
+
+
+class Revendedor(ModelBase):
+    __tablename__: str = 'revendedores'
+
+    id: int = sa.Column(
+        sa.BigInteger,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    data_criacao: datetime = sa.Column(
+        sa.DateTime,
+        default=datetime.now,
+        index=True
+    )
+
+    cnpj_cpf: str = sa.Column(
+        sa.String(45),
+        unique=True,
+        nullable=False
+    )
+
+    nome: str = sa.Column(
+        sa.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    razao_social: str = sa.Column(
+        sa.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    celular: str = sa.Column(
+        sa.String(11),
+        unique=True,
+        nullable=False
+    )
+
+    email: str = sa.Column(
+        sa.String(100),
+        unique=True,
+        nullable=False
+    )
+
+    def __repr__(self) -> str:
+        return f'<Revendedor: {self.nome}>'
