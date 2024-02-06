8914e2ab13e35b7a1e12c12f70770dcc8e4ec915
abelsalim <abelsalim@proton.me>
2024-02-06 05:08:55 -0300

    [ADD] models parte 1

---
+++ python/sqlalchemy_py/scripts/secao_03_modelagem_dados/models/ingrediente.py
@@ -0,0 +1,31 @@
+import sqlalchemy as sa
+
+from datetime import datetime
+from sqlalchemy_py.scripts.secao_03_modelagem_dados.models.models_base import (
+    ModelBase
+)
+
+
+class Ingrediente(ModelBase):
+    __tablename__: str = 'ingredientes'
+
+    id: int = sa.Column(
+        sa.BigInteger,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    data_criacao: datetime = sa.Column(
+        sa.DateTime,
+        default=datetime.now,
+        index=True
+    )
+
+    nome: str = sa.Column(
+        sa.String(45),
+        unique=True,
+        nullable=True
+    )
+
+    def __repr__(self) -> str:
+        return f'<Ingrediente: {self.nome}>'
